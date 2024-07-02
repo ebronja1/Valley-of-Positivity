eaaee2befeffdd04bfdf8df7710bf35b7eb20ef4
ebronja1 <ebronja1@etf.unsa.ba>
2024-07-02 13:02:48 +0100

    Add registration and login feature

---
+++ frontend/src/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "../../Context/useAuth";
+import { useForm } from "react-hook-form";
+import "./LoginPage.css";
+
+type Props = {};
+
+type LoginFormsInputs = {
+  userName: string;
+  password: string;
+};
+
+const validation = Yup.object().shape({
+  userName: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const LoginPage = (props: Props) => {
+  const { loginUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) });
+
+  const handleLogin = (form: LoginFormsInputs) => {
+    loginUser(form.userName, form.password);
+  };
+
+  return (
+    <section className="login-section">
+      <div className="login-container">
+        <div className="login-box">
+          <div className="login-content">
+            <h1 className="login-title">Sign in to your account</h1>
+            <form className="login-form" onSubmit={handleSubmit(handleLogin)}>
+              <div className="form-group">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="form-input"
+                  placeholder="Username"
+                  {...register("userName")}
+                />
+                {errors.userName && (
+                  <p className="error-message">{errors.userName.message}</p>
+                )}
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="form-input"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="error-message">{errors.password.message}</p>
+                )}
+              </div>
+              <div className="form-footer">
+                <a href="#" className="forgot-password">
+                  Forgot password?
+                </a>
+              </div>
+              <button type="submit" className="submit-button">
+                Sign in
+              </button>
+              <p className="signup-link">
+                Don’t have an account yet?{" "}
+                <a href="#" className="sign-up">
+                  Sign up
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
