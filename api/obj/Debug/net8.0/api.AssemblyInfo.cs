62332dce4396b6afb6a67944e08f827e9ec2356a
ebronja1 <ebronja1@etf.unsa.ba>
2024-07-02 13:01:43 +0100

    change some backend parts, enable connect from frontend part

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adeb8ee1a62338fd2985f9dafe636b81871e8dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639b120eba5a1e23865e03cb945f3c7b739ce830")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
