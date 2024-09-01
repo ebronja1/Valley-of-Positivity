654e1d250f4a059ec9611e73a61ec33c97f49783
ebronja1 <ebronja1@etf.unsa.ba>
2024-09-01 09:17:51 +0100

    ...

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b3d8b6cca79b1f1722c24d3316e758bf970844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff08126d3c81cfdf99c1fe99e2a0db3b97a59b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
