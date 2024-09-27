f18efb3b440787aae0c67edc896485cf7f13cf2d
ebronja1 <ebronja1@etf.unsa.ba>
2024-09-27 08:15:17 +0200

    dockerize

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea79e5ea1524cdfd91496ca51ccfb24ea82d55d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30908b0140784753eb9c7c349275369bb274f05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
