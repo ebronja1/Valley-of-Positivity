35c0934e084ebcce1d114d0ce4186f9b927119a1
ebronja1 <ebronja1@etf.unsa.ba>
2024-09-28 08:41:53 +0200

    seed users

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0663093863488d559387a39cdc7bebf346cb425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d99ebd01e32990bcbabecb897480211fd4ca16b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
