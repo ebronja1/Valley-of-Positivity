3bcf2eec0b57a00a68c302a89bc6eb03cc9b7139
ebronja1 <ebronja1@etf.unsa.ba>
2024-10-02 10:08:58 +0200

    add gitignore

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfd8610aaa7d6b4187f6d69a095f45a06edeff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71861ad3bdf85b61185f5aa87a92bdf2a74e31e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
