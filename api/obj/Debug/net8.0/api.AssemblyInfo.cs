23ab8d415cff529415f4dc88bc9772341630c279
ebronja1 <ebronja1@etf.unsa.ba>
2024-10-17 19:27:31 +0200

    add render yaml

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71861ad3bdf85b61185f5aa87a92bdf2a74e31e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ec2a32c795538c638ede6aad096b99168a2535")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
